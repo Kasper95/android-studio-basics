399b2b81760dc58f0324512138e3a9a8dc1f8bb7
kasper95 <kove_kasper@outlook.com>
2017-05-17 18:38:29 -0400

    refactored +FactBook class to contain facts

--- app/src/main/java/com/kasperskove/android_studio_basics/FunFactsActivity.java
+++ app/src/main/java/com/kasperskove/android_studio_basics/FunFactsActivity.java
@@ -1,13 +1,43 @@
 package com.kasperskove.android_studio_basics;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import java.util.Random;
 
 public class FunFactsActivity extends AppCompatActivity {
+
+    // view variables
+    private TextView mFactsTextView;
+    private Button mShowFactButton;
+
+    private RelativeLayout mRelativeLayout;
+    private FactBook mFactBook = new FactBook();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fun_facts);
+
+        // assinging views from the layout file to corresponding variables
+        mFactsTextView = (TextView) findViewById(R.id.factTextView);
+        mShowFactButton = (Button) findViewById(R.id.showFactButton);
+        mRelativeLayout = (RelativeLayout) findViewById(R.id.relativeLayout);
+
+        View.OnClickListener onClickListener = new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                mFactsTextView.setText(mFactBook.getFact());
+                mRelativeLayout.setBackgroundColor(Color.RED);
+            }
+        };
+
+        mShowFactButton.setOnClickListener(onClickListener);
     }
 }
